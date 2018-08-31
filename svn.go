a12a3f92728cfacd0840da54663f6b9a2a068f46
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2018-08-31 05:51:56 +0200

    feat: add list

--- svn.go
+++ svn.go
@@ -2,27 +2,65 @@ package svn
 
 import (
 	"encoding/xml"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"time"
 )
 
-type SvnList struct {
-	XMLName xml.Name       `xml:"lists"`
-	Entries []SvnListEntry `xml:"list>entry"`
+// List represents XML output of an `svn list` subcommand
+type ListElement struct {
+	XMLName xml.Name `xml:"lists"`
+	Entries []Entry  `xml:"list>entry"`
 }
 
-type SvnListEntry struct {
-	Kind   string    `xml:"kind,attr"`
-	Name   string    `xml:"name"`
-	Commit SvnCommit `xml:"commit"`
+// ListEntry represents XML output of an `svn list` subcommand
+type Entry struct {
+	Kind   string `xml:"kind,attr"`
+	Name   string `xml:"name"`
+	Commit Commit `xml:"commit"`
 }
 
-type SvnCommit struct {
+// Commit represents XML output of an `svn list` subcommand
+type Commit struct {
 	Revision string    `xml:"revision,attr"`
 	Author   string    `xml:"author"`
 	Date     time.Time `xml:"date"`
 }
 
-func svnListBzd(segment string) (SvnList, error) {
-	var l SvnList
-	return l, nil
+// Repository holds information about a (possibly remote) repository
+type Repository struct {
+	Location string
+}
+
+// NewRepository will initialize the internal structure of a possible remote
+// repository, usually pointing to the parent of the default trunk/ tags/ branches
+// structure.
+func NewRepository(l string) *Repository {
+	return &Repository{
+		Location: l,
+	}
+}
+
+// FullPath returns the full path into a repository
+func (a *Repository) FullPath(relpath string) string {
+	return fmt.Sprintf("%s/%s", a.Location, relpath)
+}
+
+// List will execute an `svn list` subcommand
+func (a *Repository) List(relpath string) ([]Entry, error) {
+	log.Printf("listing %s\n", relpath)
+	fp := a.FullPath(relpath)
+	cmd := exec.Command("svn", "list", "--xml", fp)
+	buf, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%s", buf)
+		return nil, fmt.Errorf("Cannot list %s: %s", fp, err)
+	}
+	var l ListElement
+	if err := xml.Unmarshal(buf, &l); err != nil {
+		return nil, fmt.Errorf("cannot parse XML: %s: %s", buf, err)
+	}
+	return l.Entries, nil
 }
--- svn_test.go
+++ svn_test.go
@@ -7,14 +7,28 @@ import (
 	"time"
 )
 
+// First entry from Subversion's `tags`
+func firstEntry() Entry {
+	return Entry{
+		Kind: "dir",
+		Name: "0.10.0",
+		Commit: Commit{
+			Revision: "849186",
+			Author:   "cmpilato",
+			// year, month, day, hour, minute, seconds, nanoseconds
+			Date: time.Date(2004, 3, 18, 17, 35, 35, 355023*1000, time.UTC),
+		},
+	}
+}
+
 // TestSvnList requires network access to svn.apache.org
-func TestSvnList(t *testing.T) {
+func TestOfflineList(t *testing.T) {
 	filename := "testdata/list_apache_subversion_tags.xml"
 	f, err := ioutil.ReadFile(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
-	var l SvnList
+	var l ListElement
 	if err := xml.Unmarshal(f, &l); err != nil {
 		t.Fatal(err)
 	}
@@ -27,18 +41,24 @@ func TestSvnList(t *testing.T) {
 	}
 
 	// Check first entry
-	want := SvnListEntry{
-		Kind: "dir",
-		Name: "0.10.0",
-		Commit: SvnCommit{
-			Revision: "849186",
-			Author:   "cmpilato",
-			// year, month, day, hour, minute, seconds, nanoseconds
-			Date: time.Date(2004, 3, 18, 17, 35, 35, 355023*1000, time.UTC),
-		},
-	}
+	want := firstEntry()
 	got := l.Entries[0]
 	if want != got {
+		t.Fatalf("want %q but got %q\n", want, got)
+	}
+}
+
+func TestOnlineList(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping online test in short mode.")
+	}
+	want := firstEntry()
+	r := NewRepository("https://svn.apache.org/repos/asf/subversion")
+	got, err := r.List("tags")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got[0] {
 		t.Fatalf("want %q but got %q\n", want, got)
 	}
 }
